fa1e5e0324574aa99c9fdb0de19b3e7799f2a1ab
Bernhard Ruckenstuhl <ruckenst@technikum-wien.at>
2025-09-27 15:38:51 +0200

    Präsenz 5

--- main.c
+++ main.c
@@ -1,39 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_NAME_LENGTH 32
+#define MAX_NAME_ARRAY_LENGTH (MAX_NAME_LENGTH + 1)
+
 int main()
 {
+    char name[MAX_NAME_ARRAY_LENGTH];
+    scanf("%32s", &name);
+
+    for(int i = 0; name[i] != '\0' && i < MAX_NAME_ARRAY_LENGTH; i++) {
+        printf("%c", name[i]);
+    }
+
     int xPos = 0;
     int yPos = 0;
 
-    printf("x: %d, y: %d\n", xPos, yPos);
+    int isRunning = 1;
 
-    char direction;
-    scanf("%c", &direction);
+    while(isRunning) {
+        printf("x: %d, y: %d\n", xPos, yPos);
 
-    switch(direction) {
-        case 'W':
-        case 'w':
-            yPos++;
-            break;
-        case 'A':
-        case 'a':
-            xPos--;
-            break;
-        case 'S':
-        case 's':
-            yPos--;
-            break;
-        case 'D':
-        case 'd':
-            xPos++;
-            break;
-        default:
-            printf("Oh no! You entered something stupid!\n");
-            break;
+        int isInvalidInput;
+        do {
+            char direction;
+            scanf(" %c", &direction);
+
+            isInvalidInput = 0;
+
+            switch(direction) {
+                case 'W':
+                case 'w':
+                    yPos++;
+                    break;
+                case 'A':
+                case 'a':
+                    xPos--;
+                    break;
+                case 'S':
+                case 's':
+                    yPos--;
+                    break;
+                case 'D':
+                case 'd':
+                    xPos++;
+                    break;
+                case 'X':
+                case 'x':
+                    isRunning = 0;
+                    break;
+                default:
+                    printf("Oh no! You entered something stupid!\n");
+                    isInvalidInput = 1;
+                    break;
+            }
+        } while(isInvalidInput);
     }
-
-    printf("x: %d, y: %d\n", xPos, yPos);
 
     return 0;
 }
