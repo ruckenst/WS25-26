c498ea3188d24e7e5c8ddab7d3c6f8da5d7e2273
Bernhard Ruckenstuhl <ruckenst@technikum-wien.at>
2025-09-27 15:30:54 +0200

    Präsenz 4

--- main.c
+++ main.c
@@ -3,28 +3,43 @@
 
 int main()
 {
-    int x = 123;
-    if(x == 234){
-        printf("true\n");
-    } else {
-        printf("false\n");
+    int t = 123;
+    int isRunning = 1;
+
+    for(int i = 0; i < 100; i++) {
+        printf("%d\n", i);
     }
 
-    float temperature;
-    printf("Enter the current temperature in degree celsius: ");
-    scanf("%f", &temperature);
+    int xPos = 0;
+    int yPos = 0;
 
-    printf("The temperature is: %.1f Degree Celsius\n", temperature);
+    while() {
+        printf("x: %d, y: %d\n", xPos, yPos);
 
+        char direction;
+        scanf("%c", &direction);
 
-    if (temperature < 15) {
-        printf("It is cold :(\n");
-    }
-    else if (temperature >= 25 && temperature < 33) {
-        printf("It is warm :)\n");
-    }
-    else if (temperature >= 33) {
-        printf("It is hot :'(\n");
+        switch(direction) {
+            case 'W':
+            case 'w':
+                yPos++;
+                break;
+            case 'A':
+            case 'a':
+                xPos--;
+                break;
+            case 'S':
+            case 's':
+                yPos--;
+                break;
+            case 'D':
+            case 'd':
+                xPos++;
+                break;
+            default:
+                printf("Oh no! You've entered garbage\n");
+                break;
+        }
     }
 
     return 0;
