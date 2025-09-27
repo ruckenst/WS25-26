e4b792e8c7efc186f0340510bc2dd61e70c11818
Bernhard Ruckenstuhl <ruckenst@technikum-wien.at>
2025-09-27 15:31:57 +0200

    Präsenz 6

--- main.c
+++ main.c
@@ -4,53 +4,103 @@
 #define MAX_NAME_LENGTH 16
 #define MAX_NAME_ARRAY_LENGTH (MAX_NAME_LENGTH + 1)
 
+int spellString(char string[], int stringLength);
+int move(char direction, int* xPos, int* yPos);
+void incrementByReference(int* i);
+int incrementByValue(int i);
+void A();
+void B();
+
 int main()
 {
+    A();
+    B();
+
     char name[MAX_NAME_ARRAY_LENGTH];
     scanf("%16s", &name);
 
-    for(int i = 0; i < MAX_NAME_ARRAY_LENGTH; i++) {
-        printf("%c", name[i]);
-    }
-
+    spellString(name, MAX_NAME_ARRAY_LENGTH);
 
     int xPos = 0;
     int yPos = 0;
 
-    while(1) {
+    while(1)
+    {
         printf("x: %d, y: %d\n", xPos, yPos);
 
         int isInvalidInput;
-        do {
+        do
+        {
             char direction;
             scanf(" %c", &direction);
 
-            isInvalidInput = 0;
-
-            switch(direction) {
-                case 'W':
-                case 'w':
-                    yPos++;
-                    break;
-                case 'A':
-                case 'a':
-                    xPos--;
-                    break;
-                case 'S':
-                case 's':
-                    yPos--;
-                    break;
-                case 'D':
-                case 'd':
-                    xPos++;
-                    break;
-                default:
-                    printf("Oh no! You've entered garbage\n");
-                    isInvalidInput = 1;
-                    break;
-            }
-        } while(isInvalidInput);
+            isInvalidInput = move(direction, &xPos, &yPos);
+        }
+        while(isInvalidInput);
     }
 
     return 0;
+}
+
+int spellString(char string[], int stringLength)
+{
+    for(int charIndex = 0; charIndex < stringLength; charIndex++)
+    {
+        if(string[charIndex] == '\0') {
+            return charIndex;
+        }
+
+        printf("%c\n", string[charIndex]);
+    }
+
+    return stringLength;
+}
+
+int move(char direction, int* xPos, int* yPos)
+{
+    switch(direction)
+    {
+        case 'W':
+        case 'w':
+            (*yPos)++;
+            break;
+        case 'A':
+        case 'a':
+            (*xPos)--;
+            break;
+        case 'S':
+        case 's':
+            (*yPos)--;
+            break;
+        case 'D':
+        case 'd':
+            (*xPos)++;
+            break;
+        default:
+            printf("Oh no! You've entered garbage\n");
+            return 1;
+    }
+
+    return 0;
+}
+
+void incrementByReference(int* i) {
+    (*i)++;
+    //*i = *i + 1;
+    //*i += 1;
+}
+
+int incrementByValue(int i) {
+    i++;
+
+    return i;
+}
+
+void A() {
+    B();
+    printf("A\n");
+}
+
+void B() {
+    printf("B\n");
 }
