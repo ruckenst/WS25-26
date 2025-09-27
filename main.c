2db67547e514eff3e4190b0d236a231a4508a135
Bernhard Ruckenstuhl <ruckenst@technikum-wien.at>
2025-09-27 15:31:25 +0200

    Präsenz 5

--- main.c
+++ main.c
@@ -1,45 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_NAME_LENGTH 16
+#define MAX_NAME_ARRAY_LENGTH (MAX_NAME_LENGTH + 1)
+
 int main()
 {
-    int t = 123;
-    int isRunning = 1;
+    char name[MAX_NAME_ARRAY_LENGTH];
+    scanf("%16s", &name);
 
-    for(int i = 0; i < 100; i++) {
-        printf("%d\n", i);
+    for(int i = 0; i < MAX_NAME_ARRAY_LENGTH; i++) {
+        printf("%c", name[i]);
     }
+
 
     int xPos = 0;
     int yPos = 0;
 
-    while() {
+    while(1) {
         printf("x: %d, y: %d\n", xPos, yPos);
 
-        char direction;
-        scanf("%c", &direction);
+        int isInvalidInput;
+        do {
+            char direction;
+            scanf(" %c", &direction);
 
-        switch(direction) {
-            case 'W':
-            case 'w':
-                yPos++;
-                break;
-            case 'A':
-            case 'a':
-                xPos--;
-                break;
-            case 'S':
-            case 's':
-                yPos--;
-                break;
-            case 'D':
-            case 'd':
-                xPos++;
-                break;
-            default:
-                printf("Oh no! You've entered garbage\n");
-                break;
-        }
+            isInvalidInput = 0;
+
+            switch(direction) {
+                case 'W':
+                case 'w':
+                    yPos++;
+                    break;
+                case 'A':
+                case 'a':
+                    xPos--;
+                    break;
+                case 'S':
+                case 's':
+                    yPos--;
+                    break;
+                case 'D':
+                case 'd':
+                    xPos++;
+                    break;
+                default:
+                    printf("Oh no! You've entered garbage\n");
+                    isInvalidInput = 1;
+                    break;
+            }
+        } while(isInvalidInput);
     }
 
     return 0;
