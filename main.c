4e57257ec1696a971ac5dd340e0f7082b3c2eeb7
Zumpel96 <17179371+Zumpel96@users.noreply.github.com>
2025-09-30 15:52:12 +0200

    Präsenz 7

--- main.c
+++ main.c
@@ -1,106 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_NAME_LENGTH 16
-#define MAX_NAME_ARRAY_LENGTH (MAX_NAME_LENGTH + 1)
+void increment(int* value);
+int isValidDate(int day, int month, int year);
+int isValidYear(int year);
+int isValidMonth(int month);
+int isValidDay(int day);
 
-int spellString(char string[], int stringLength);
-int move(char direction, int* xPos, int* yPos);
-void incrementByReference(int* i);
-int incrementByValue(int i);
-void A();
-void B();
+int main() {
+    int i = 123;
+    int day = 30;
+    int month = 9;
+    int year = 2025;
 
-int main()
-{
-    A();
-    B();
-
-    char name[MAX_NAME_ARRAY_LENGTH];
-    scanf("%16s", &name);
-
-    spellString(name, MAX_NAME_ARRAY_LENGTH);
-
-    int xPos = 0;
-    int yPos = 0;
-
-    while(1)
-    {
-        printf("x: %d, y: %d\n", xPos, yPos);
-
-        int isInvalidInput;
-        do
-        {
-            char direction;
-            scanf(" %c", &direction);
-
-            isInvalidInput = move(direction, &xPos, &yPos);
-        }
-        while(isInvalidInput);
+    if(isValidDate(day, month, year)) {
+        printf("TRUE\n");
+    } else {
+        printf("FALSE\n");
     }
 
-    return 0;
+    increment(&i);
+    printf("%d\n", i);
 }
 
-int spellString(char string[], int stringLength)
-{
-    for(int charIndex = 0; charIndex < stringLength; charIndex++)
-    {
-        if(string[charIndex] == '\0') {
-            return charIndex;
-        }
+void increment(int* value) {
+    *value += 1;
+}
 
-        printf("%c\n", string[charIndex]);
+int isValidDate(int day, int month, int year) {
+    printf("isValidDate\n");
+    return isValidDay(day) && isValidMonth(month) && isValidYear(year);
+}
+
+int isValidYear(int year) {
+    printf("isValidYear\n");
+    return year >= 1900 && year <= 2100;
+}
+
+int isValidMonth(int month) {
+    /*if(month >= 1 && month <= 12) {
+        return 1;
     }
 
-    return stringLength;
+    return 0;*/
+
+    printf("isValidMonth\n");
+    return month >= 1 && month <= 12;
 }
 
-int move(char direction, int* xPos, int* yPos)
-{
-    switch(direction)
-    {
-        case 'W':
-        case 'w':
-            (*yPos)++;
-            break;
-        case 'A':
-        case 'a':
-            (*xPos)--;
-            break;
-        case 'S':
-        case 's':
-            (*yPos)--;
-            break;
-        case 'D':
-        case 'd':
-            (*xPos)++;
-            break;
-        default:
-            printf("Oh no! You've entered garbage\n");
-            return 1;
-    }
-
-    return 0;
-}
-
-void incrementByReference(int* i) {
-    (*i)++;
-    //*i = *i + 1;
-    //*i += 1;
-}
-
-int incrementByValue(int i) {
-    i++;
-
-    return i;
-}
-
-void A() {
-    B();
-    printf("A\n");
-}
-
-void B() {
-    printf("B\n");
+int isValidDay(int day) {
+    printf("isValidDay\n");
+    return 1;
 }
