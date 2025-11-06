758c2039d5d465034bbf9060b22dc02cf0a8b06e
Bernhard Ruckenstuhl <ruckenst@technikum-wien.at>
2025-11-06 17:22:34 +0100

    Präsenz 14

--- main.c
+++ main.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
+int divide(int a, int b) {
+    return a / b;
+}
 
 enum Gender {
     Female,
@@ -7,111 +24,80 @@ enum Gender {
     Diverse
 };
 
+enum Grade {
+    Ungraded,
+    Excellent,
+    Good,
+    Ok,
+    Poor,
+    Failed
+};
+
+struct Lecture {
+    char       Name[33]; // 32 + \0
+    enum Grade Grade;
+};
+
 struct Student {
-    char Name[33]; // 32 + \0
-    enum Gender Gender;
+    char           Name[33]; // 32 + \0
+    struct Lecture Lectures[10];
+    int            NumberOfLectures;
+    enum Gender    Gender;
 };
 
-
-
-
-
-
-enum Weekday {
-    Sunday,
-    Monday,
-    Tuesday,
-    Wednesday,
-    Thursday,
-    Friday,
-    Saturday
-};
-
-void printWeekday(enum Weekday day) {
-    switch(day) {
-    case Monday:
-        printf("Monday\n");
-        break;
-    case Tuesday:
-        printf("Tuesday\n");
-        break;
-    case Wednesday:
-        printf("Wednesday\n");
-        break;
-    case Thursday:
-        printf("Thursday\n");
-        break;
-    case Friday:
-        printf("Friday\n");
-        break;
-    case Saturday:
-        printf("Saturday\n");
-        break;
-    case Sunday:
-        printf("Sunday\n");
-        break;
+void addLectureToStudent(struct Student* student,
+                         char* lectureName,
+                         enum Grade grade) {
+    if(student->NumberOfLectures >= 10) {
+        printf("The student has reached their maximum number of lectures!\n");
+        return;
     }
-}
 
-struct Coordinate {
-    int X;
-    int Y;
-    int Z;
-};
-
-void printCoordinate(struct Coordinate coord) {
-    printf("{ x = %d, y = %d, z = %d }\n", coord.X, coord.Y, coord.Z);
-}
-
-int addInteger(int integerA, int integerB) {
-    int result = integerA + integerB;
-
-    return result;
-}
-
-struct Coordinate addCoordinates(struct Coordinate coordA, struct Coordinate coordB) {
-    struct Coordinate result = {
-        .X = coordA.X + coordB.X,
-        .Y = coordA.Y + coordB.Y,
-        .Z = coordA.Z + coordB.Z
+    struct Lecture newLecture = {
+        .Grade = grade
     };
+    strcpy(newLecture.Name, lectureName);
 
-    return result;
-}
-
-void increment(int* i) {
-    (*i)++;
-}
-
-//increment(&i);
-
-void moveUp(struct Coordinate* coord) {
-    //(*coord).Y++;
-    coord->Y++;
+    student->Lectures[student->NumberOfLectures] = newLecture;
+    student->NumberOfLectures++;
 }
 
 int main()
 {
-    int i = 123;
-    struct Coordinate myCoord = {
-        .X = 12,
-        .Y = 53,
-        .Z = 9
+    struct Student myStudent = {
+        .Name = "Sepp",
+        .Gender = Male,
+        .NumberOfLectures = 1,
+        .Lectures = {
+            {
+                .Name = "Prozedurale Sprachen",
+                .Grade = Excellent
+            }
+        }
     };
 
-    printCoordinate(myCoord);
-    moveUp(&myCoord);
-    printCoordinate(myCoord);
-
-    printWeekday(Tuesday);
-
-    printf("%d\n", Monday);
-    printf("%d\n", Tuesday);
-    printf("%d\n", Wednesday);
-    printf("%d\n", Thursday);
-    printf("%d\n", Friday);
-    printf("%d\n", Saturday);
-    printf("%d\n", Sunday);
+    addLectureToStudent(&myStudent,
+                        "Grundlagen der Informatik",
+                        Poor);
 
     return 0;
+
+    /*
+    int valueA, valueB;
+    int option;
+    int (*operations[])(int, int) = { &add, &subtract, &multiply, &divide };
+
+    printf("Enter value a: ");
+    scanf("%d", &valueA);
+
+    printf("Enter value b: ");
+    scanf("%d", &valueB);
+
+    printf("Enter your option (0 = Add, 1 = Subtract, 2 = Multiply, 3 = Divide): ");
+    scanf("%d", &option);
+
+    int result = (*operations[option])(valueA, valueB);
+    printf("Result: %d\n", result);
+    return 0;
+    */
 }
