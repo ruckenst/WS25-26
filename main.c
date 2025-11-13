e2db0fd8c4ca9ff123b413ae4885be1d04f11633
Bernhard Ruckenstuhl <ruckenst@technikum-wien.at>
2025-11-13 16:07:59 +0100

    Präsenz 16

--- main.c
+++ main.c
@@ -1,28 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Something {
-    int age;
-    char name[100];
+struct node {
+    int value;
+    struct node* next;
 };
+
+struct node* createNode(int value) {
+    struct node* newNode = malloc(sizeof(struct node));
+    printf("Node Created (%d)\n", value);
+
+    newNode->value = value;
+    newNode->next = NULL;
+
+    return newNode;
+};
+
+void freeNode(struct node* node) {
+    int value = node->value;
+
+    free(node);
+    printf("Node Freed (%d)\n", value);
+}
+
+void printNode(struct node* node) {
+    //printf("value: %d\n", node->value);
+    //printf("next:  %d\n", node->next);
+
+    printf("%d -> ", node->value);
+}
+
+void freeList(struct node* head) {
+    struct node* current = head;
+
+    while(current != NULL) {
+        struct node* next = current->next;
+        freeNode(current);
+
+        current = next;
+    }
+}
+
+void freeListRecursive(struct node* head) {
+    if(head == NULL) {
+        return;
+    }
+
+    struct node* next = head->next;
+    freeNode(head);
+
+    freeListRecursive(next);
+}
+
+void printList(struct node* head) {
+    struct node* current = head;
+
+    while(current != NULL) {
+        printNode(current);
+        current = current->next;
+    }
+
+    printf("NULL\n");
+}
+
+void printListRecursive(struct node* head) {
+    if(head == NULL) {
+        printf("NULL\n");
+        return;
+    }
+
+    printNode(head);
+    printListRecursive(head->next);
+}
 
 int main()
 {
-    int i = 123;
-    int* heapI = malloc(10 * sizeof(int));
-    if(heapI == NULL) {
-        printf("Oh no! Not enough memory :(\n");
-        return 1;
-    }
+    struct node* head = NULL;
+    printList(head);
 
-    for(int x = 0; x < 10; x++) {
-        heapI[x] = x;
-    }
+    head = createNode(12);
+    head->next = createNode(27);
+    freeList(head);
 
-    for(int x = 0; x < 10; x++) {
-        printf("heapI = %d\n", heapI[x]);
-    }
+    head = createNode(123);
 
-    free(heapI);
+    printList(head);
+    printListRecursive(head);
+
+    freeList(head);
+
     return 0;
 }
