90ce04f70bc2a3a1fe58d801fe4b1b5fc9669863
Bernhard Ruckenstuhl <ruckenst@technikum-wien.at>
2025-11-13 14:31:57 +0100

    Präsenz 15

--- main.c
+++ main.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+struct Something {
+    int age;
+    char name[100];
+};
+
+int main()
+{
+    int i = 123;
+    int* heapI = malloc(10 * sizeof(int));
+    if(heapI == NULL) {
+        printf("Oh no! Not enough memory :(\n");
+        return 1;
+    }
+
+    for(int x = 0; x < 10; x++) {
+        heapI[x] = x;
+    }
+
+    for(int x = 0; x < 10; x++) {
+        printf("heapI = %d\n", heapI[x]);
+    }
+
+    free(heapI);
+    return 0;
+}
