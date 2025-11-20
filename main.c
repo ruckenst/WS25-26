3739b74669c6fa00dfe8d8f47b688d9f9ee5566c
Bernhard Ruckenstuhl <ruckenst@technikum-wien.at>
2025-11-20 16:09:04 +0100

    Präsenz 18

--- main.c
+++ main.c
@@ -7,15 +7,14 @@ struct node {
 };
 
 void printNode(struct node* node) {
-    //printf("value: %d\n", node->value);
-    //printf("next:  %d\n", node->next);
-
     printf("%d -> ", node->value);
 }
 
 struct node* initializeNode(int value) {
     struct node* newNode = malloc(sizeof(struct node));
+    #ifdef DEBUG_PRINT
     printf("Node initialized (%d)\n", value);
+    #endif
 
     newNode->value = value;
     newNode->next = NULL;
@@ -26,7 +25,10 @@ struct node* initializeNode(int value) {
 void freeNode(struct node* node) {
     int value = node->value;
     free(node);
+
+    #ifdef DEBUG_PRINT
     printf("Node freed (%d)\n", value);
+    #endif
 }
 
 void printList(struct node* head) {
@@ -71,17 +73,121 @@ void freeListRecursive(struct node* head) {
     freeListRecursive(next);
 }
 
+void insertBack(struct node** head, struct node* newNode) {
+    // Check if list is empty
+    if(*head == NULL) {
+        *head = newNode;
+        return;
+    }
+
+    struct node* current = *head;
+
+    // Go to the last valid element
+    while(current->next != NULL) {
+        current = current->next;
+    }
+
+    current->next = newNode;
+}
+
+void insertFront(struct node** head, struct node* newNode) {
+    newNode->next = *head;
+    *head = newNode;
+}
+
+void insertSorted(struct node** head, struct node* newNode) {
+    // Check if list is empty
+    if(*head == NULL) {
+        *head = newNode;
+        return;
+    }
+
+    struct node* previous = NULL;
+    struct node* current = *head;
+
+    // Go to the last valid element or the first bigger element
+    while(current != NULL &&
+          current->value < newNode->value) {
+        previous = current;
+        current = current->next;
+    }
+
+    if(previous == NULL) {
+        insertFront(head, newNode);
+        return;
+    }
+
+    previous->next = newNode;
+    newNode->next = current;
+}
+
+void removeAt(struct node** head, int index) {
+    // Check for empty list
+    if(*head == NULL) {
+        printf("List is empty!\n");
+        return;
+    }
+
+    // Check if the first element needs to be removed
+    if(index == 0) {
+        struct node* nodeToRemove = *head;
+        *head = (*head)->next;
+
+        freeNode(nodeToRemove);
+        return;
+    }
+
+    int currentIndex = 0;
+    struct node* previous = NULL;
+    struct node* current = *head;
+
+    // Go to the last valid element or until the index is hit
+    while(current->next != NULL && currentIndex < index) {
+        previous = current;
+        current = current->next;
+
+        currentIndex++;
+    }
+
+    // Check if the index was hit
+    if(currentIndex != index) {
+        printf("Index was out of bounds!\n");
+        return;
+    }
+
+    struct node* nodeToRemove = current;
+    previous->next = current->next;
+    freeNode(nodeToRemove);
+}
+
 int main()
 {
     struct node* head = NULL;
+    removeAt(&head, 0);
     printListRecursive(head);
 
-    head = initializeNode(7);
-    head->next = initializeNode(9);
-    freeList(head);
+    insertSorted(&head, initializeNode(7));
+    insertSorted(&head, initializeNode(3));
+    insertSorted(&head, initializeNode(5));
 
-    head = initializeNode(83);
+    removeAt(&head, 0);
+
+    printListRecursive(head);
+
+    return 0;
+
+    for(int i = 0; i < 10; i++) {
+        insertBack(&head, initializeNode(i));
+    }
 
     printList(head);
-    freeListRecursive(head);
+
+    for(int i = 0; i < 10; i++) {
+        insertFront(&head, initializeNode(i));
+    }
+
+    printList(head);
+    freeList(head);
+
+    return 0;
 }
