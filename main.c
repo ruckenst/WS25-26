cbd26a70c83438a71c87299dfa18d223bef8dc1a
Bernhard Ruckenstuhl <ruckenst@technikum-wien.at>
2025-12-04 14:30:08 +0100

    Präsenz 21

--- main.c
+++ main.c
@@ -1,54 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-struct employee {
-    char firstname[33];
-    char lastname[33];
-    int id;
-};
+#define ARRAY_SIZE 100
+#define MAX_VALUE 50
+
+void printArray(int arr[], int size) {
+    printf("[");
+    for(int i = 0; i < size; i++) {
+        printf("%d", arr[i]);
+
+        if(i < size - 1) {
+            printf(", ");
+        }
+    }
+    printf("]\n");
+}
+
+void initializeArray(int arr[], int size) {
+    for(int i = 0; i < size; i++) {
+        int factor = i * (i + size);
+        int primedFactor = factor % 19 * (i + 13);
+        int shiftedFactor = primedFactor  >> 3;
+
+        arr[i] = shiftedFactor % MAX_VALUE;
+    }
+}
+
+void bubbleSortArray(int arr[], int count) {
+    for(int i = 0; i < count - 1; i++) {
+        for(int j = 0; j < count /*- i*/ - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void bubbleSortArrayVisualgo(int arr[], int count) {
+    int swapped = 0;
+    int sortedCount = 0;
+
+    do {
+        swapped = 0;
+        for(int i = 0; i < count - sortedCount - 1; i++) {
+            if(arr[i] > arr[i + 1]) {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+
+                swapped = 1;
+            }
+        }
+
+        sortedCount++;
+    }while(swapped);
+}
 
 int main()
 {
-    struct employee* employees[10];
-    for(int i = 0; i < 10; i++) {
-        employees[i] = malloc(sizeof(struct employee));
-        strcpy(employees[i]->firstname, "Name A");
-        strcpy(employees[i]->lastname, "Name B");
-        employees[i]->id = i;
-    }
+    int arr[ARRAY_SIZE];
+    initializeArray(arr, ARRAY_SIZE);
+    printArray(arr, ARRAY_SIZE);
 
-    FILE* newFile = fopen("./files/Export.csv", "w");
-    if(newFile == NULL) {
-        printf("File could not be opened!\n");
-        return 1;
-    }
+    bubbleSortArray(arr, ARRAY_SIZE);
+    //bubbleSortArrayVisualgo(arr, ARRAY_SIZE);
 
-    for(int i = 0; i < 10; i++) {
-        fprintf(newFile, "%s,%s,%d\n",
-                employees[i]->firstname,
-                employees[i]->lastname,
-                employees[i]->id);
-    }
-
-    fclose(newFile);
-
-    for(int i = 0; i < 10; i++) {
-        free(employees[i]);
-    }
-
-    FILE* file = fopen("./files/Test.csv", "r");
-    if(file == NULL) {
-        printf("File could not be opened!\n");
-        return 1;
-    }
-
-    char line[65];
-    while(fscanf(file, "%64s\n", &line) != EOF) {
-        printf("%s\n", line);
-    }
-
-    fclose(file);
+    printArray(arr, ARRAY_SIZE);
 
     return 0;
 }
